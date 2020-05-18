820a0451504c73100f6f960300d2a0f580788403
Karthick <karthick111.kk@gmail.com>
2020-05-18 14:12:03 +0530

    Navigation Component and Bottom navigation view Setup

--- app/src/main/java/com/example/androidmastery/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/androidmastery/ui/main/view/MainActivity.kt
@@ -2,12 +2,40 @@ package com.example.androidmastery.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.androidmastery.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun setupViews() {
+        // Finding the Navigation Controller
+        var navController = findNavController(R.id.fragNavHost)
+
+        // Setting Navigation Controller with the BottomNavigationView
+        bottomNavView.setupWithNavController(navController)
+
+        // Setting Up ActionBar with Navigation Controller
+        // Pass the IDs of top-level destinations in AppBarConfiguration
+        var appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(
+                R.id.homeFragment,
+                R.id.browseFragment,
+                R.id.interestFragment,
+                R.id.notificationFragment,
+                R.id.profileFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
